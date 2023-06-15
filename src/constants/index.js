e7e3dbc5a6a54b24ffe652a839fb4605013cafc1
Evans <48053069+daveevans04@users.noreply.github.com>
2023-06-15 19:39:40 +0800

    Updates

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+import icons from './icons';
+import images from './images';
+import theme, {COLORS, SIZES, FONTS} from './theme';
+import Data from './data';
+
+export {icons, images, theme, COLORS, SIZES, FONTS, Data};
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,51 @@
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+  FlatList,
+} from 'react-native';
+import {FONTS, COLORS, SIZES, icons, images, Data} from '../constants';
+import {CategoryCard} from '../components';
 import React from 'react';
 
-const Home = () => {
+const Home = ({navigation}) => {
+  // Navigation
+  const onRecipePressed = item => {
+    navigation.navigate('Recipe', {recipe: item});
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+      <FlatList
+        data={Data.categories}
+        keyExtractor={item => `${item.id}`}
+        keyboardDismissMode="on-drag"
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponentStyle={
+          <View>
+            {/* Header */}
+            {renderHeader()}
+            {/* Search Bar */}
+            {/* See Recipe Card */}
+            {/* Trending Section */}
+            {/* Category Header */}
+          </View>
+        }
+        renderItem={({item}) => {
+          return (
+            <CategoryCard
+              categoryItem={item}
+              containerStyle={{marginHorizontal: SIZES.padding}}
+              onPress={onRecipePressed}
+            />
+          );
+        }}
+        ListFooterComponent={<View style={{marginBottom: 100}} />}
+      />
+    </SafeAreaView>
   );
 };
 
